10599bcdaccbcc4c8c9832bac0e6640e637eae4c
Reinis Tilaks <reinistak@youforce.net>
2020-02-25 11:50:14 +0200

    Updated RegEx to work for Vārds and Uzvārds. Also edited display name for both models.

--- Skola-Forma/Models/Lietotaji.cs
+++ Skola-Forma/Models/Lietotaji.cs
@@ -10,11 +10,13 @@ namespace Skola_Forma.Models
     {
         [Required(ErrorMessage = "Šajā laukā ir jāievada dati.")]
         [MinLength(3, ErrorMessage = "Vārdam jābūt vismaz 30 simbolus garam."), MaxLength(30, ErrorMessage = "Vārdam jābūt maksimāli 30 simbolus garam.")]
-        [RegularExpression("^(?!.*[a-zA-ZĒŪĪŌĀŠĢĶĻŽČŅēūīōāšģķļžčņ]).*$", ErrorMessage = "Drīkst ievadīt tikai burtus.")]
+        [RegularExpression("^[a-zA-ZĒŪĪŌĀŠĢĶĻŽČŅēūīōāšģķļžčņ]+$", ErrorMessage = "Drīkst ievadīt tikai burtus.")]
+        [Display(Name = "Vārds")]
         public string Vards { get; set; }
         [Required(ErrorMessage = "Šajā laukā ir jāievada dati.")]
         [MinLength(3, ErrorMessage = "Uzvārdam jābūt vismaz 3 simbolus garam."), MaxLength(30, ErrorMessage = "Uzvārdam jābūt maksimāli 30 simbolus garam.")]
-        [RegularExpression("^(?!.*[a-zA-ZĒŪĪŌĀŠĢĶĻŽČŅēūīōāšģķļžčņ]).*$", ErrorMessage = "Drīkst ievadīt tikai burtus.")]
+        [RegularExpression("^[a-zA-ZĒŪĪŌĀŠĢĶĻŽČŅēūīōāšģķļžčņ]+$", ErrorMessage = "Drīkst ievadīt tikai burtus.")]
+        [Display(Name = "Uzvārds")]
         public string Uzvards { get; set; }
         [Required(ErrorMessage = "Šajā laukā ir jāievada dati.")]
         [MinLength(10, ErrorMessage = "Adresei jābūt vismaz 10 simbolus garai."), MaxLength(150, ErrorMessage = "Adresei jābūt maksimāli 150 simbolus garai.")]
