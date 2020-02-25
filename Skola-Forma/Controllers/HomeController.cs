667cdfd9cdbd9d2852191dfcc7c4ccdd6ad7e299
Reinis Tilaks <reinistak@youforce.net>
2020-02-25 09:49:28 +0200

    Otrais darbs, viss izņemot normāli strādājošu RegEx priekš Vārda un Uzvārda - Toerētiski jāļauj tikai Lielie un mazie burti, bet atļauj ciparus.

---
+++ Skola-Forma/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Skola_Forma.Models;
+
+namespace Skola_Forma.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public IConfiguration Configuration { get; }
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            Configuration = configuration;
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public IActionResult Create(Lietotaji lietotaji)
+        {
+            if (ModelState.IsValid)
+            {
+                string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string sql = $"Insert Into [Table] (Vards, Uzvards, Adrese, Telefons) Values (@Vards,@Uzvards,@Adrese,@Telefons)";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@Vards", lietotaji.Vards);
+                        command.Parameters.AddWithValue("@Uzvards", lietotaji.Uzvards);
+                        command.Parameters.AddWithValue("@Adrese", lietotaji.Adrese);
+                        command.Parameters.AddWithValue("@Telefons", lietotaji.Telefons);
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+                return View();
+        }
+
+
+        public IActionResult Index()
+        {
+            List<Lietotaji> lietotajiList = new List<Lietotaji>();
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"]; using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                //SqlDataReader
+                connection.Open();
+
+                string sql = "Select * From [Table]";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                using (SqlDataReader dataReader = command.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        Lietotaji lietotaji = new Lietotaji();
+                        lietotaji.Vards = Convert.ToString(dataReader["Vards"]);
+                        lietotaji.Uzvards = Convert.ToString(dataReader["Uzvards"]);
+                        lietotaji.Adrese = Convert.ToString(dataReader["Adrese"]);
+                        lietotaji.Telefons = Convert.ToInt32(dataReader["Telefons"]);
+                        lietotajiList.Add(lietotaji);
+                    }
+                }
+                connection.Close();
+            }
+            return View(lietotajiList);
+        }
+    } 
+}
---
+++ Skola-Forma/Models/Lietotaji.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Skola_Forma.Models
+{
+    public class Lietotaji
+    {
+        [Required(ErrorMessage = "Šajā laukā ir jāievada dati.")]
+        [MinLength(3, ErrorMessage = "Vārdam jābūt vismaz 30 simbolus garam."), MaxLength(30, ErrorMessage = "Vārdam jābūt maksimāli 30 simbolus garam.")]
+        [RegularExpression("^(?!.*[a-zA-ZĒŪĪŌĀŠĢĶĻŽČŅēūīōāšģķļžčņ]).*$", ErrorMessage = "Drīkst ievadīt tikai burtus.")]
+        public string Vards { get; set; }
+        [Required(ErrorMessage = "Šajā laukā ir jāievada dati.")]
+        [MinLength(3, ErrorMessage = "Uzvārdam jābūt vismaz 3 simbolus garam."), MaxLength(30, ErrorMessage = "Uzvārdam jābūt maksimāli 30 simbolus garam.")]
+        [RegularExpression("^(?!.*[a-zA-ZĒŪĪŌĀŠĢĶĻŽČŅēūīōāšģķļžčņ]).*$", ErrorMessage = "Drīkst ievadīt tikai burtus.")]
+        public string Uzvards { get; set; }
+        [Required(ErrorMessage = "Šajā laukā ir jāievada dati.")]
+        [MinLength(10, ErrorMessage = "Adresei jābūt vismaz 10 simbolus garai."), MaxLength(150, ErrorMessage = "Adresei jābūt maksimāli 150 simbolus garai.")]
+        public string Adrese { get; set; }
+        [Required(ErrorMessage = "Šajā laukā ir jāievada dati.")]
+        [RegularExpression("^[0-9]{8}$", ErrorMessage = "Telefona numuram jābūt 8 ciparus garam.")]
+        public int Telefons { get; set; }
+    }
+}
